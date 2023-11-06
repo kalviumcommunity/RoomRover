29de03ef2c6b1304814d3b6cb405ee2c417b75ca
Akanksha987 <agrawalakanksha23123@gmail.com>
2023-11-06 21:14:10 +0530

    Milestone for CA-4

--- models.py
+++ models.py
@@ -1,28 +1,27 @@
 from pydantic import BaseModel
 
-class Hotel(BaseModel):
-    hotel_id: int
+class BaseEntity(BaseModel):
+    entity_id: int
+
+class Hotel(BaseEntity):
     name: str
     address: str
     phone: str
     email: str
-    MAX_ROOMS:int = 100 
+    MAX_ROOMS: int = 100
 
-class RoomType(BaseModel):
-    room_type_id: int
+class RoomType(BaseEntity):
     type_name: str
     description: str
 
-class Room(BaseModel):
-    room_id: int
+class Room(BaseEntity):
     hotel_id: int
     room_number: str
     room_type_id: int
     price: float
     is_available: bool
 
-class Guest(BaseModel):
-    guest_id: int
+class Guest(BaseEntity):
     first_name: str
     last_name: str
     address: str
@@ -30,24 +29,17 @@ class Guest(BaseModel):
     email: str
     no_of_adults: int
     no_of_children: int
+    MAX_PEOPLE_IN_ROOM: int = 10
 
-    MAX_PEOPLE_IN_ROOM: int = 10  
-
-class Payment(BaseModel):
-    payment_id: int
+class Payment(BaseEntity):
     reservation_id: int
     payment_amount: float
     payment_date: str
 
-class Reservation(BaseModel):
-    reservation_id: int
+class Reservation(BaseEntity):
     guest_id: int
     hotel_id: int
     room_id: int
     check_in_date: str
     check_out_date: str
     total_amount: float
-
-    def __del__(self):
-        print(f"Reservation {self.reservation_id} deleted.")
-
